3b8d30b7cd6b10389b8653fa25f5f4c3da2994ca
Daniels Viļčuks <da61@inbox.lv>
2023-11-13 22:21:12 +0200

    update

--- Cv.Testss/Cv_map_test.cs
+++ Cv.Testss/Cv_map_test.cs
@@ -23,7 +23,6 @@ public class MappingTests
     [TestMethod]
     public void Test_CurriculumVitae_To_CvItemModel_Mapping()
     {
-        // Arrange
         var curriculumVitae = new CurriculumVitae
         {
             Id = 1,
@@ -59,39 +58,34 @@ public class MappingTests
             }
         };
 
-        // Act
+       
         var cvItemModel = _mapper.Map<CurriculumVitae, CvItemModel>(curriculumVitae);
 
-        // Assert
+        
         Assert.AreEqual(curriculumVitae.Id, cvItemModel.Id);
         Assert.AreEqual(curriculumVitae.FirstName, cvItemModel.Name);
         Assert.AreEqual(curriculumVitae.LastName, cvItemModel.LastName);
         Assert.AreEqual(curriculumVitae.PhoneNumber, cvItemModel.PhoneNumber);
         Assert.AreEqual(curriculumVitae.Email, cvItemModel.Email);
 
-        // Assert LanguageKnowledge
         Assert.AreEqual(curriculumVitae.Languages.Count, cvItemModel.LanguageKnowledge.Count);
         Assert.AreEqual(curriculumVitae.Languages.First().Language, cvItemModel.LanguageKnowledge.First().Language);
         Assert.AreEqual(curriculumVitae.Languages.First().LanguageLevel, cvItemModel.LanguageKnowledge.First().LanguageLevel);
 
-        // Assert Skills
         Assert.AreEqual(curriculumVitae.Skills.Count, cvItemModel.Skills.Count);
         Assert.AreEqual(curriculumVitae.Skills.First().SkillName, cvItemModel.Skills.First().SkillName);
         Assert.AreEqual(curriculumVitae.Skills.First().SkillDescription, cvItemModel.Skills.First().SkillDescription);
 
-        // Assert Educations
         Assert.AreEqual(curriculumVitae.Educations.Count, cvItemModel.Educations.Count);
         Assert.AreEqual(curriculumVitae.Educations.First().EducationalLevel, cvItemModel.Educations.First().EducationalLevel);
         Assert.AreEqual(curriculumVitae.Educations.First().NameOfSchool, cvItemModel.Educations.First().NameOfSchool);
         Assert.AreEqual(curriculumVitae.Educations.First().StudyStartDate, cvItemModel.Educations.First().StudyStartDate);
 
-        // Assert Employments
         Assert.AreEqual(curriculumVitae.Employments.Count, cvItemModel.Employments.Count);
         Assert.AreEqual(curriculumVitae.Employments.First().NameOfCompany, cvItemModel.Employments.First().NameOfCompany);
         Assert.AreEqual(curriculumVitae.Employments.First().JobPosition, cvItemModel.Employments.First().JobPosition);
         Assert.AreEqual(curriculumVitae.Employments.First().StartDate, cvItemModel.Employments.First().StartDate);
 
-        // Assert Address
         Assert.AreEqual(curriculumVitae.Address.PostCode, cvItemModel.Address.PostCode);
         Assert.AreEqual(curriculumVitae.Address.City, cvItemModel.Address.City);
         Assert.AreEqual(curriculumVitae.Address.Street, cvItemModel.Address.Street);
@@ -102,7 +96,6 @@ public class MappingTests
     [TestMethod]
     public void Test_Address_To_AddressViewModel_Mapping()
     {
-        // Arrange
         var address = new Address
         {
             PostCode = "12345",
@@ -113,10 +106,8 @@ public class MappingTests
             CurriculumVitaeId = 1
         };
 
-        // Act
         var addressViewModel = _mapper.Map<Address, AddressViewModel>(address);
 
-        // Assert
         Assert.AreEqual(address.PostCode, addressViewModel.PostCode);
         Assert.AreEqual(address.City, addressViewModel.City);
         Assert.AreEqual(address.Street, addressViewModel.Street);
